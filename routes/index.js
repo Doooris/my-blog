b3a14f8a5bd0e2aaa37d60100bcf215a0c53cc2f
Doooris <hxpt2031@gmail.com>
2016-11-03 00:22:06 +0800

    first tpl ajust

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+/**
+ * Created by sitin on 16/11/3.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '博客首页', name: '博客'});
 });
 
 module.exports = router;
---
+++ routes/update.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sitin on 16/11/3.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET update page. */
+router.get('/update', function(req, res, next) {
+  res.render('update', { title: '博客更新页', name: '博客更新'});
+});
+
+module.exports = router;
