796ecafd414f1c8906116d50cb2a18b3d3122625
Doooris <hxpt2031@gmail.com>
2017-03-04 00:20:55 +0800

    update app.js file

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use('/article', article_tags);
 app.use('/article', article_category);
 app.use('/picture',picture);
 app.use('/about',about);
-app.use(auth.connect(basic));
+app.use('/admin',auth.connect(basic));
 app.use('/admin', admin_index);
 app.use('/admin', admin_update);
 
