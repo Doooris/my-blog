edb1022b91634c0609bfbe6b3628b439b4a5b01f
Doooris <hxpt2031@gmail.com>
2017-04-06 22:31:37 +0800

    last modified to make the nav visible

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,16 +16,16 @@ $(function() {
       $(".odd-section").css({"background-color":"white","background":"linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.8)"});
     }
   });
-  $(".menubarH").mouseenter(function(){
-    $(this).stop().fadeOut(0).siblings(".fa-lightbulb-o").animate({left:"5%"},400)
-          .siblings(".content-nav").css("visibility","visible").animate({left:"20%"},400)
-      });
-  $("nav").mouseleave(function(){
-    $(".content-nav").animate({left:"95%"},400,function(){
-      $(this).css("visibility","hidden")
-        .siblings(".menubarH").stop().fadeIn(400)
-    })
-      .siblings(".fa-lightbulb-o").animate({left:"68%"},400)
-  })
+  // $(".menubarH").mouseenter(function(){
+  //   $(this).stop().fadeOut(0).siblings(".fa-lightbulb-o").animate({left:"5%"},400)
+  //         .siblings(".content-nav").css("visibility","visible").animate({left:"20%"},400)
+  // //     });
+  // $("nav").mouseleave(function(){
+  //   $(".content-nav").animate({left:"95%"},400,function(){
+  //     $(this).css("visibility","hidden")
+  //       .siblings(".menubarH").stop().fadeIn(400)
+  //   })
+  //     .siblings(".fa-lightbulb-o").animate({left:"68%"},400)
+  // })
   })
 
